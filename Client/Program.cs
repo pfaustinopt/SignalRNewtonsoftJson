4daa7d5b88b060600b0b1f8e5baba0be7c3fc685
Pedro Faustino <pedro.faustino@medicineone.net>
2021-03-11 12:07:19 +0000

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = new Uri("http://localhost:5000/test");
+
+            var clinicalEvicendeHub = new HubConnectionBuilder()
+                .WithUrl(url)
+                .AddNewtonsoftJsonProtocol()
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                })
+                .Build();
+
+            clinicalEvicendeHub.StartAsync().Wait();
+            Console.WriteLine("Connected!");
+            Console.ReadLine();
+        }
+    }
+}
